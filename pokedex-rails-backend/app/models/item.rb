7a2add78410404a4640380e4cfcceb93413aba40
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-01-30 17:28:03 -0500

    complete phase 1

--- pokedex-rails-backend/app/models/item.rb
+++ pokedex-rails-backend/app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
-  validates :name, length: { less_than: 255 }
+  validates :name, length: { maximum: 254 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :happiness, :image_url, presence: true
 
   belongs_to :pokemon,
-    primary_id: :id,
+    primary_key: :id,
     foreign_key: :pokemon_id,
     class_name: :Pokemon
 end
--- pokedex-rails-backend/app/models/pokemon.rb
+++ pokedex-rails-backend/app/models/pokemon.rb
@@ -20,17 +20,17 @@ class Pokemon < ApplicationRecord
 
     validates :attack, :defense, numericality: { in: 0..100 }
     validates :poke_type, inclusion: { in: TYPES, message: "%{value} is not a valid Pokemon type"}
-    validates :image_url, presence: true,
+    validates :image_url, presence: true
 
     validates :number, numericality: { greater_than: 0 }, uniqueness: { message: "%{value} is already in use"}
     validates :name, length: { in: 3..255 }, uniqueness: { message: "%{value} is already in use"}
-    validates :captured, inclusion: [true, false]
+    validates :captured, inclusion: { in: [true, false], message: "Captured must be true or false" }
 
     has_many :poke_moves,
         dependent: :destroy
 
     has_many :moves,
-        through: :poke_moves,
+        through: :poke_moves
 
     has_many :items,
         foreign_key: :pokemon_id
